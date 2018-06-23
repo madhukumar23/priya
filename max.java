1743fda7eb751fb4bd00ce4621f2c49176fc636a
madhukumar23 <madhunila8@gmail.com>
2018-06-23 11:05:57 +0530

    Create max.java

---
+++ max.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class max
+{
+public static void main(String args[])
+ {
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    int n1=s.nextInt();
+    int n2=s.nextInt();
+			if ((n1>n)&&(n1>n2)) 
+			{
+				System.out.print(n1);
+			}
+			if ((n>n1)&&(n>n2)) 
+			{
+				System.out.print(n);
+			}
+			if ((n2>n)&&(n2>n1)) 
+			{
+				System.out.pr
